b53921ed70a6d5c1078d5bac2dcf166e82f90e1d
jnlsn01 <jnlsn01@gmail.com>
2019-12-05 20:28:17 -0500

    Update runme.js

--- runme.js
+++ runme.js
@@ -1,6 +1,7 @@
 var start = 147981;
 var end = 691423;
 var passdiff = 0;
+var passdiff2 = 0;
 const diffarray = [];
 
 for(let current=start; current<=end; current++){
@@ -23,8 +24,45 @@ for(let current=start; current<=end; current++){
 								if (check5 <= check6){
 									if (check5 === check6) doublecheck = 1;
 									if (doublecheck === 1) {
+										let diffarray2 = [];
 										passdiff++;
-										diffarray.push(current);
+										diffarray2.push(check1, check2, check3, check4, check5, check6);
+										let met = 0;
+										if (diffarray2[0] == diffarray2[1]) {
+											if (diffarray2[1] != diffarray2[2]) {
+												met = 1;
+												passdiff2++;
+											}
+										}
+										if (met ===0) {
+										if (diffarray2[1] == diffarray2[2]) {
+											if ((diffarray2[0] != diffarray2[1]) && (diffarray2[2] != diffarray2[3])) {
+												met = 1;
+												passdiff2++;
+											}
+										}
+										}
+										if (met ===0) {
+										if (diffarray2[2] == diffarray2[3]) {
+											if ((diffarray2[1] != diffarray2[2]) && (diffarray2[3] != diffarray2[4])) {
+												met = 1;
+												passdiff2++;
+											}
+										}
+										}
+										if (met ===0) {
+										if (diffarray2[3] == diffarray2[4]) {
+											if ((diffarray2[2] != diffarray2[3]) && (diffarray2[4] != diffarray2[5])) {
+												met = 1;
+												passdiff2++;
+											}
+										}
+										}
+										if (met ===0) {
+										if (diffarray2[4] == diffarray2[5]) {
+											if (diffarray2[3] != diffarray2[4]) passdiff2++;
+										}
+										}
 									}
 								}
 							}
@@ -33,62 +71,5 @@ for(let current=start; current<=end; current++){
             }
 };
 
-var passdiff2 = 0;
-for(mynum of diffarray) {
-	let singlecheck = 0;
-	let doublecheck = 0;
-	let triplecheck = 0;
-	let quadcheck = 0;
-	let fifthcheck = 0;
-	//let sixthcheck = 0;
-	let mystring = mynum.toString();
-	let check1 = Number(mystring.substr(0,1));
-	let check2 = Number(mystring.substr(1,1));
-	let check3 = Number(mystring.substr(2,1));
-	let check4 = Number(mystring.substr(3,1));
-	let check5 = Number(mystring.substr(4,1));
-	let check6 = Number(mystring.substr(5,1));
-	
-	if (check1 === check2) {
-		singlecheck++;
-		if (check1 === check3){
-			singlecheck++;
-			if (check1 === check4) {
-				singlecheck++;
-				if (check1 === check5) {
-					singlecheck++;
-					if (check1 === check6) singlecheck++;
-				}
-			}
-		}
-	}
-	if (check2 === check3){
-		doublecheck++;
-		if (check2 === check4) {
-			doublecheck++;
-			if (check2 === check5) {
-				doublecheck++;
-				if (check2 === check6) doublecheck++;
-			}
-		}
-	}
-	if (check3 === check4) {
-		triplecheck++;
-		if (check3 === check5) {
-			triplecheck++;
-			if (check3 === check6) triplecheck++;
-		}
-	}
-	if (check4 === check5) {
-		quadcheck++;
-		if (check4 === check6) quadcheck++;
-	}
-	if (check5 === check6) fifthcheck++;
-
-	if ((singlecheck === 2) || (doublecheck === 2) || (triplecheck === 2) || (quadcheck === 2) || (fifthcheck === 2)) passdiff2++;
-};
-
-//157799
-//console.log(diffarray);
 console.log("Part 1: " , passdiff);
 console.log("Part 2: " , passdiff2);
